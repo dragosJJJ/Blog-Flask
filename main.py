8a0618409ce68efa3d5f7432695e57ac241ce35f
dragosJJJ <dragos_juganariu@yahoo.com>
2023-12-06 00:22:57 +0200

    a

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
+new_post = Post()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", post=new_post)
 
-@app.route('/post/<num>')
+@app.route('/post/<int:num>')
 def get_post(num):
-
-    return render_template("post.html")
+    post_data = new_post.getPost(num)
+    return render_template("post.html", num=num, post_data = post_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- post.py
+++ post.py
@@ -1,9 +1,8 @@
 import requests
 
 class Post:
-    def __init__(self, num):
+    def __init__(self):
         self.response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
-        self.num = num
 
     def getPost(self, num):
-        return self.response.json()[num]
+        return self.response.json()[num-1]
